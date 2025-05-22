d7d0e32c2af954ecae39919888f8e475a9049937
luis-Saberon <luis.b.r.saberon@gmail.com>
2025-05-21 22:48:46 -0700

    Fixed timer bug

--- public/js/card.js
+++ public/js/card.js
@@ -34,10 +34,11 @@ function powerUp() {
 
 function gameOver(win) {
   const x = Object.values(document.getElementById('holder').classList)
+  timer = 99999;
   console.log(`/game/${x[0]}/${x[1]}`)
   if(win) {
     if (confirm("You won, play again?")) {
-      window.location.href=`game/${x[0]}/${x[1]}`
+      window.location.href=`/game/${x[0]}/${x[1]}`
     } else { 
       window.location.href=`/${x[0]}`
     }
